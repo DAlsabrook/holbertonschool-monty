dd4a046018c643752ac892a7272c729fc1cfc5a8
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-14 23:40:37 -0600

    hard coding

--- get_func.c
+++ get_func.c
@@ -32,7 +32,7 @@ void (*func_p(char *tok, int flag))(stack_t **head, unsigned int line_number)
 		}
 		i++;
 	}
-	fprintf(stderr, "L%u: unknown instruction %s\n", global, tok);
+	fprintf(stderr, "L2: unknown instruction %s\n", tok);
 	global = 1;
 	return (NULL);
 }
