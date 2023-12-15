53c46b3aadd8d87dc09dd598c78743c2d3a2d53a
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-15 12:44:37 -0600

    bug fix for add.c

--- add.c
+++ add.c
@@ -10,8 +10,9 @@ void add(stack_t **head, unsigned int line_number)
 {
 	if (!*head || !(*head)-> next)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		global = 10000;
+		return;
 	}
 	(*head)->next->n += (*head)->n;
 	pop(head, line_number);
