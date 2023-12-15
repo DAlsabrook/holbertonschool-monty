b71dec6d009660e706e1e75857cd94a33e16c207
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2023-12-15 00:51:15 -0600

    added swap

---
+++ swap.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+/**
+ * swap - swap top two nodes
+ * 
+ * 
+ * 
+ * Return: void
+*/
+void nop(stack_t **head, unsigned int line_number)
+{
+	stack_t *tmp, *tmp2;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		global = 1;
+		return;
+	}
+
+	tmp = *head;
+	tmp2 = tmp->next;
+
+	tmp->next = tmp2->next;
+	if (tmp2->next != NULL)
+	{
+		tmp2->next->prev = tmp;
+	}
+
+	tmp2->prev = NULL;
+	tmp2->next = tmp;
+	tmp->prev = tmp2;
+
+	*head = tmp2;
+}
